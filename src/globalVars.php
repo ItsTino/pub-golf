9dbe892ecaeb6c7ab0ff23ed154103761c4f13fd
ItsTino <duvalvalentino@gmail.com>
2021-12-24 00:27:36 +0000

    Create globalVars.php
    Add course info

---
+++ src/globalVars.php
@@ -0,0 +1,14 @@
+<?php
+//Predefine the course list
+//This is cheating but enough database stuff for now and this isn't dynamic yet
+$courseList[1] = array('step' => '1', 'name' => 'The Lark Rise', 'drink' => 'Lager', 'par' => '3');
+$courseList[2] = array('step' => '2', 'name' => 'Parkstone & Heatherlands', 'drink' => 'Lager', 'par' => '3');
+$courseList[3] = array('step' => '3', 'name' => 'Buffalo', 'drink' => 'Cider', 'par' => '3');
+$courseList[4] = array('step' => '4', 'name' => 'Richmond Arms', 'drink' => 'Lager', 'par' => '3');
+$courseList[5] = array('step' => '5', 'name' => 'Tesco Express', 'drink' => 'Tinny', 'par' => '3');
+$courseList[6] = array('step' => '6', 'name' => 'Christopher Creeke', 'drink' => 'Spirit x2', 'par' => '2');
+$courseList[7] = array('step' => '7', 'name' => 'George Tapps', 'drink' => 'Wine', 'par' => '2');
+$courseList[8] = array('step' => '8', 'name' => 'Brasshouse', 'drink' => 'Dark Fruit', 'par' => '3');
+$courseList[9] = array('step' => '9', 'name' => 'Moon', 'drink' => 'Pitcher (1x per Team)', 'par' => '3');
+$courseList[10] = array('step' => '10', 'name' => 'Shelley', 'drink' => 'Lager', 'par' => '3');
+?>

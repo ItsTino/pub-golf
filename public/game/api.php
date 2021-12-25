a22284a0f45b5499716b45ca1533c24b907b8a68
ItsTino <duvalvalentino@gmail.com>
2021-12-25 15:04:41 +0000

    Update api.php

--- public/game/api.php
+++ public/game/api.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Alert if game is in progress (Joining in progress game is not planned at this time) 
     if ($_POST['action'] == 'joingame') {
         //Check game code to see if it exists
-        
+
         $joinID = filter_var($_POST['joinID'], FILTER_SANITIZE_STRING);
         if ($stmt = mysqli_prepare($dbconnect, "SELECT * FROM tblGameInfo WHERE gameJoinCode=?")) {
 
@@ -52,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Return 2 => done
     if ($_POST['action'] == 'checkgamestatus') {
         $gameID = filter_var($_POST['gameID'], FILTER_SANITIZE_STRING);
-        
+
         if ($stmt = mysqli_prepare($dbconnect, "SELECT * FROM tblGameInfo WHERE uGameID=?")) {
 
             /* bind parameters for markers */
@@ -65,49 +65,67 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             while ($row = $result->fetch_all(MYSQLI_BOTH)) {
                 foreach ($row as $r) {
                     $gameStatusNo = $r['gameStatus'];
-                    
                 }
-                
+
                 switch ($gameStatusNo) {
-                    case '0':                    
-                      echo 'waiting';
-                      break;
+                    case '0':
+                        echo 'waiting';
+                        break;
                     case '1':
-                      echo 'started';
-                      break;
+                        echo 'started';
+                        break;
                     case '2':
-                      echo 'done';
-                      break;
+                        echo 'done';
+                        break;
                     default:
-                     echo('error');      
-                  }
+                        echo ('error');
+                }
             }
+        }
     }
-}
 
     //Get Pre-Game Lobby Information
     //This is going to return players in the game and what team they're in
     if ($_POST['getpregameinfo']) {
-        class pregameplayer{
+        class pregameplayer
+        {
             public $uuID;
             public $uDisplayName;
             public $teamID;
             public $teamName;
         }
-    
+
         $pregameplayer = array();
-
     }
+    if ($_POST['action'] == 'jointeam') {
+        //Check game code to see if it exists
+        $uuID = filter_var($_POST['uuID'], FILTER_SANITIZE_STRING);
+        $gameID = filter_var($_POST['gameID'], FILTER_SANITIZE_STRING);
+        $teamID = filter_var($_POST['teamID'], FILTER_SANITIZE_STRING);
+        if ($stmt = mysqli_prepare($dbconnect, "SELECT * FROM tblGameInfo WHERE gameJoinCode=?")) {
 
+            /* bind parameters for markers */
+            $stmt->bind_param("s", $joinID);
 
+            $stmt->execute();
 
+            $result = $stmt->get_result();
 
-
- } else {
-     echo "Hi, you shouldn't be accessing this directly :)";
+            while ($row = $result->fetch_all(MYSQLI_BOTH)) {
+                foreach ($row as $r) {
+                    $_SESSION['gameInviteCode'] = $joinID;
+                    $_SESSION['currentGameSession'] = $r['uGameID'];
+                    if ($_SESSION['uuID'] == $r['gameOwnerID']) {
+                        $_SESSION['isMod'] = true;
+                    } else {
+                        $_SESSION['isMod'] = false;
+                    }
+                    echo 'success';
+                }
+            }
+        }
+    }
+} else {
+    echo "Hi, you shouldn't be accessing this directly :)";
     exit();
- }
-
-
-
-?>
+}

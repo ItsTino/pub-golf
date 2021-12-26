9723d100f343528855a5e8b7d9e4682f6e0b41f5
ItsTino <duvalvalentino@gmail.com>
2021-12-26 02:57:42 +0000

    Update api.php
    Finish POST API for base game

--- public/game/api.php
+++ public/game/api.php
@@ -84,45 +84,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    //Get Pre-Game Lobby Information
-    //This is going to return players in the game and what team they're in
-    if ($_POST['getpregameinfo']) {
-        class pregameplayer
-        {
-            public $uuID;
-            public $uDisplayName;
-            public $teamID;
-            public $teamName;
-        }
-
-        $pregameplayer = array();
-    }
-    if ($_POST['action'] == 'jointeam') {
-        //Check game code to see if it exists
-        $uuID = filter_var($_POST['uuID'], FILTER_SANITIZE_STRING);
+    if ($_POST['action'] == 'incRound') {
         $gameID = filter_var($_POST['gameID'], FILTER_SANITIZE_STRING);
-        $teamID = filter_var($_POST['teamID'], FILTER_SANITIZE_STRING);
-        if ($stmt = mysqli_prepare($dbconnect, "SELECT * FROM tblGameInfo WHERE gameJoinCode=?")) {
+
+        if ($stmt = mysqli_prepare($dbconnect, "UPDATE tblGameInfo SET `gameRound` = `gameRound` + 1 WHERE `uGameID`=?")) {
 
             /* bind parameters for markers */
-            $stmt->bind_param("s", $joinID);
+            $stmt->bind_param("s", $gameID);
 
             $stmt->execute();
 
-            $result = $stmt->get_result();
+            echo 'success';
 
-            while ($row = $result->fetch_all(MYSQLI_BOTH)) {
-                foreach ($row as $r) {
-                    $_SESSION['gameInviteCode'] = $joinID;
-                    $_SESSION['currentGameSession'] = $r['uGameID'];
-                    if ($_SESSION['uuID'] == $r['gameOwnerID']) {
-                        $_SESSION['isMod'] = true;
-                    } else {
-                        $_SESSION['isMod'] = false;
-                    }
-                    echo 'success';
-                }
-            }
+        }
+    }
+
+    if ($_POST['action'] == 'saveScore') {
+        $gameID = filter_var($_POST['gameID'], FILTER_SANITIZE_STRING);
+        $teamID = filter_var($_POST['teamID'], FILTER_SANITIZE_STRING);
+        $round = filter_var($_POST['round'], FILTER_SANITIZE_STRING);
+        $score = filter_var($_POST['score'], FILTER_SANITIZE_STRING);
+
+        if ($stmt = mysqli_prepare($dbconnect, "UPDATE `tblRoundScore` SET `rsScore` = ? WHERE `rsGameID` = ? AND `rsTeamID` = ? AND `rsRound` = ?")) {
+
+            /* bind parameters for markers */
+            $stmt->bind_param("issi", $score, $gameID, $teamID, $round);
+
+            $stmt->execute();
+
+            echo 'success';
+
         }
     }
 } else {
